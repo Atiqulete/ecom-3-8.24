de102ec5053836acd919bf81dbc9dc02ee0b281e
Atiqulete <atiqulete@gmail.com>
2024-08-10 14:58:30 +0600

    cat

--- website/views.py
+++ website/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render,redirect
 from website.forms import Contacfrom
-from website.models import Banner
+from website.models import Banner,Category,Brand,Product
 
-# Create your views here.
+# Create your views here.,
 
 def index(request):
     banner = Banner.objects.all()
+    category = Category.objects.all()
+    brand = Brand.objects.all()
+    product = Product.objects.all()
+    
     context = {
-
-        'banner' : banner
+        'banner' : banner,
+        'category' : category,
+        'brand' : brand,
+        'product' : product
     }
+
     return render(request,'website/index.html',context)
 
 def contact(request):
